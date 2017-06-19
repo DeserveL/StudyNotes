cfd0edb6b08723b706e49d5ad92e880e5f788d63
DeverseL <714395012@qq.com>
2017-06-19 10:36:43 +0800

    工厂方法模式

---
+++ basic/src/main/java/com/liuchong/studynotes/designpatterns/factorymethod/base/Creator.java
@@ -0,0 +1,10 @@
+package com.liuchong.studynotes.designpatterns.factorymethod.base;
+
+/**
+ * @author DeserveL
+ * @date 2017/6/19 10:30
+ * @since 1.0.0
+ */
+public interface Creator {
+    Light creatLight();
+}
